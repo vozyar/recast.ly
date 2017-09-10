f8ccb3878a14bda4cebba5fab36382e0f9a87346
Volkan <Home@Volkan.local>
2017-09-10 16:20:22 -0700

    try

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,19 +1,13 @@
-/*var VideoList = () => (
+var VideoList = ({videos, handleVideoListEntryTitleClick}) => (
   <div className="video-list">
-    <VideoListEntry />
-     <VideoListEntry = exampleVideoData.map(function (item, index) {
-      return item;
-    })/>*/
-
-var VideoList = (props) => (
-  <div className="video-list">
-    {props.videos.map((video, index)=> (
-      <VideoListEntry video={video} key={index}/>
-    )
+    {videos.map((video) =>
+      <VideoListEntry
+        key={video.etag}
+        video={video}
+        handleVideoListEntryTitleClick={handleVideoListEntryTitleClick}
+      />
     )}
-    
-   
-  </div> 
+  </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
@@ -25,12 +19,3 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
-
-
-// var VideoList = (props) => (
-//   <ul>
-//     {props.exampleVideoData.map( VideoListEntry=>
-//       <VideoList VideoListEntry={VideoListEntry} />
-//     )}
-//   </ul>
-// );
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,12 @@
-var VideoPlayer = (props) => (
-  <div>
+var VideoPlayer = ({video}) => (
+  !video ? <div className="video-player">Please wait...</div> :
+  <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={'https://www.youtube.com/embed/' + props.video.id.videoId} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>{props.video.snippet.title}</h3>
-      <div>{props.video.snippet.description}</div>
+      <h3>{video.snippet.title}</h3>
+      <div>{video.snippet.description}</div>
     </div>
   </div>
 );
@@ -19,7 +20,3 @@ VideoPlayer.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.VideoPlayer = VideoPlayer;
-
-
-
-
